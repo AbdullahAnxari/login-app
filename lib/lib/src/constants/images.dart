6dedda180f5c9791130a344df60a1405a03ae576
AbdullahAnxari <abdullah012304@gmail.com>
2023-12-10 07:18:41 +0500

    login app

---
+++ lib/lib/src/constants/images.dart
@@ -0,0 +1,25 @@
+/* -- App Image Strings -- */
+
+
+// -- Splash Screen Images
+const String tSplashTopIcon = "assets/images/delivery.jpg";
+const String tSplashImage = "assets/images/delivery.jpg";
+
+
+// -- OnBoarding Screen Images
+const String tOnBoardingImage1 = "assets/images/delivery.jpg";
+const String tOnBoardingImage2 = "assets/images/delivery.jpg";
+const String tOnBoardingImage3 = "assets/images/delivery.jpg";
+
+
+// -- Welcome Screen Images
+// -- also used in Login & SignUp
+const String tWelcomeScreenImage = "assets/images/delivery.jpg";
+
+
+// -- Login & SignUp Images
+const String tGoogleLogoImage = "assets/images/delivery.jpg";
+
+
+// -- Forget Password Images
+const String tForgetPasswordImage = "assets/images/delivery.jpg";
